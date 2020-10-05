826b84730d552217b36b091a1d25c6d76a094065
Demola Ayokanmbi <aoa0006@uah.edu>
2020-10-05 18:01:30 -0500

    Example 2, Drawing arcs and text with QEllipse and QPoint

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Graphics Example 1
+// Graphics Example 2
 
 #include <QApplication>
 #include <QPainter>
@@ -6,80 +6,58 @@
 #include <QPen>
 #include <QBrush>
 #include <QRect>
+#include <QPoint>
+#include <QLine>
+#include <QFont>
 #include <QLabel>
+
+	// Goal: Draw a sniper crosshair along
+	// with the word "Fire"
 
 int main(int argc, char* argv[])
 
 {
 	QApplication myApp(argc, argv);				
 	
-QPixmap myMap(400,300);									// Establish 400 x 300 pixmap
+	QPixmap myMap(400,300);								// Establish 400 x 300 pixmap
+	myMap.fill(Qt::black);								// Makes sure background is black
 	
 	QPainter p(&myMap);									// QPainter class attached to QPixmap
 	p.setRenderHint(QPainter::Antialiasing, true);		// Enable antialiasing
 	
-	// Draw rect frame ('380 x 280' vs. '400 x 300')
-	p.setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::SquareCap));
-	p.drawRect(10,10,380,280);							// Drawing begins at (10, 10)
+	// Draw four points and two crosshair lines
+	p.setPen(QPen(Qt::red, 10, Qt::SolidLine, Qt::FlatCap));
+	QPoint p1(200,25);			// West
+	QPoint p2(200,275);			// East
+	QPoint p3(50, 150);			// North
+	QPoint p4(350, 150);		// South
+	p.drawPoint(p1);
+	p.drawPoint(p2);
+	p.drawPoint(p3);
+	p.drawPoint(p4);
+	p.setPen(QPen(Qt::white, 5, Qt::SolidLine, Qt::FlatCap));
+	QLine line1(p1, p2);
+	QLine line2(p3, p4);
+	p.drawLine(line1);
+	p.drawLine(line2);
 	
+	// Define Bounding Rectangle, set pen, and draw ellipse
+	p.setPen(QPen(Qt::blue, 5, Qt::SolidLine, Qt::SquareCap));
+	QRect rect1(125,75,150,150);						// 150 x 150 circle at (125,75)
+	p.drawEllipse(rect1);
 	
-	// Vary Cap Style and Pen Color
-	p.setPen(QPen(Qt::blue, 10, Qt::SolidLine, Qt::SquareCap));	// Blue and Square Cap
-	QRect rect1(25,25,50,30);							// New 50 x 30 QRect object at (25,25)
-	p.drawRect(rect1);
+	// Write text
+	p.setPen(Qt::red);
+	QPoint tp(300,175);
+	p.drawText(tp, "Fire");
 	
-	p.setPen(QPen(Qt::red, 10, Qt::SolidLine, Qt::RoundCap));	// Red and Round Cap
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
+	// Draw arcs --- angles are 1/16 of a degree, hence the scale factor
+	p.setPen(Qt::green);
+	p.drawArc(QRect(100, 50, 200, 200), 00*16, 90*16);	// Rectangle defines ellipse
+	p.drawArc(QRect(100, 50, 200, 200), 180*16, 90*16);	// second arg is start angle
+	p.drawArc(QRect(100, 50, 200, 200), 120*16, 30*16);	// third arg is span size of angle
+	p.drawArc(QRect(100, 50, 200, 200), 300*16, 30*16);
 	
-	p.setPen(QPen(Qt::green, 10, Qt::SolidLine, Qt::FlatCap));	// Green and Flat Cap
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	
-	// Vary Join Style
-	p.setPen(QPen(Qt::blue, 10, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin));	// Blue and MiterJoin
-	rect1.translate(-200,75);							// Translate rect left by 200 and down 75
-	p.drawRect(rect1);									
-	
-	p.setPen(QPen(Qt::red, 10, Qt::SolidLine, Qt::FlatCap, Qt::BevelJoin));	// Red and BevelJoin
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	p.setPen(QPen(Qt::green, 10, Qt::SolidLine, Qt::FlatCap, Qt::RoundJoin));	// Green and RoundJoin
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	
-	// Vary Join Style
-	p.setPen(QPen(Qt::blue, 2, Qt::DashLine, Qt::SquareCap));	// Blue and Dashed Line
-	rect1.translate(-200,75);							// Translate rect left by 200 and down 75
-	p.drawRect(rect1);									
-	
-	p.setPen(QPen(Qt::red, 2, Qt::DotLine, Qt::RoundCap));	// Red and Dotted Line
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	p.setPen(QPen(Qt::green, 2, Qt::DashDotLine, Qt::FlatCap));	// Green and Dash/Dotted Line
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	
-	// Vary Brush Style
-	p.setPen(QPen(Qt::blue, 2, Qt::SolidLine, Qt::SquareCap));	// Blue
-	p.setBrush(QBrush(Qt::blue, Qt::SolidPattern));				// and Solid Pattern Brush
-	rect1.translate(-200,75);							// Translate rect left by 200 and down 75
-	p.drawRect(rect1);									
-	
-	p.setPen(QPen(Qt::red, 2, Qt::SolidLine, Qt::RoundCap));		// Red
-	p.setBrush(QBrush(Qt::red, Qt::DiagCrossPattern));			// and Diagonal Cross Brush
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
-	
-	p.setPen(QPen(Qt::green, 2, Qt::SolidLine, Qt::FlatCap));	// Green
-	p.setBrush(QBrush(Qt::green, Qt::VerPattern));				// and Vertical Pattern Brush
-	rect1.translate(100,0);								// Translate rect right by 100
-	p.drawRect(rect1);
 	
 	// Create QLabel and set Pixmap into QLabel
 	QLabel myLabel;
